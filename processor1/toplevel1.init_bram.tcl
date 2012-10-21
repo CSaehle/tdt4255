71454eb9d805bfc8e44dec80fc4c195cc4315c23
Christer <chribru@stud.ntnu.no>
2012-10-21 13:53:59 +0200

    cleanup

--- processor1/toplevel1.init_bram.tcl
+++
@@ -1,8 +0,0 @@
-cd C:/Users/chribru/Desktop/GitHub/tdt4255/processor1/toplevel1
-if { [ catch { xload xmp toplevel1.xmp } result ] } {
-  exit 10
-}
-if { [catch {run init_bram} result] } {
-  exit -1
-}
-exit 0
